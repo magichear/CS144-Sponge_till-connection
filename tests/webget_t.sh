1d2e796e092f2977bec15bad66bb7ed9976180c6
zhangshengqi <zsq@educg.net>
2021-12-21 15:12:48 +0800

    Replace webget_t.sh

--- tests/webget_t.sh
+++ tests/webget_t.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-/headless/sponge/tap.sh start
+#/headless/sponge/tap.sh start
 service apache2 start
 echo "QWx0NhMPkoM/bJr/ohvHXlviFhOyYrYb+qqdOnwLYo4"  > /var/www/html/xyzzy
-WEB_HASH=`./apps/webget 169.254.10.1 /xyzzy | tee /dev/stderr | tail -n 1`
+WEB_HASH=`./apps/webget 127.0.0.1 /xyzzy | tee /dev/stderr | tail -n 1`
 CORRECT_HASH="QWx0NhMPkoM/bJr/ohvHXlviFhOyYrYb+qqdOnwLYo4"
 
 if [ "${WEB_HASH}" != "${CORRECT_HASH}" ]; then
